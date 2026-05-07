716e5242788c5b68971386c75de0c8fd5239e275
Norberto Luna <norberto.luna@piconsulting.com.ar>
2026-05-07 20:30:36 -0300

    [FIX] Fixed foundrys test

--- app/tests/foundrys.py
+++ app/tests/foundrys.py
@@ -9,22 +9,27 @@ def foundrys_tests(data: list[dict]) -> dict:
     if item is None or 'ok' in item:
       continue
     
-    if 'router' in item['partial_answers']:
+    if 'router' in item['partial_answers'] and 'grounding' in item['partial_answers']:
       node_metadata = item['node_metadata']
       route = item['partial_answers'].get('router', {'route': ''}).get('route', '')
       
-      ref_link = node_metadata.get('reformulate', '').get('endpoint', '').split(':')[0]
-      tri_link = node_metadata.get('triage').get('endpoint', '').split(':')[0]
+      ref_link = node_metadata.get('reformulate').get('endpoint_routing')[0].get('endpoint').split(':')[0]
+      tri_link = node_metadata.get('triage').get('endpoint_routing')[0].get('endpoint', '').split(':')[0]
     
-      
-      rou_link = node_metadata.get('router').get('endpoint', '').split(':')[0]
-      per_link = node_metadata.get('personality', {'endpoint': 'x'}).get('endpoint', 'x').split(':')[0]
-      gro_link = node_metadata.get('grounding', {'endpoint': 'x'}).get('endpoint', 'x').split(':')[0]
-      
-      ree_link = node_metadata.get(f'agent_{route}', {'retrieve_embeddings': {'endpoint': 'x'}}).get('retrieve_embeddings',{'endpoint': 'x'}).get('endpoint', 'x').split(':')[0]
-      rag_link = node_metadata.get(f'agent_{route}', {'rag_answer': {'endpoint': 'x'}}).get('rag_answer',{'endpoint': 'x'}).get('endpoint', 'x').split(':')[0]
+      rou_link = '99'
 
+      if node_metadata['semantic_router']['endpoint_routing'][0].get('endpoint'):
+        rou_link = node_metadata['semantic_router']['endpoint_routing'][0].get('endpoint').split(':')[0]
+      else:
+        node_metadata['llm_router']['endpoint_routing'][0].get('endpoint').split(':')[0]
+
+
+      per_link = node_metadata.get('personality', {'endpoint': 'x'}).get('endpoint_routing')[0].get('endpoint', 'x').split(':')[0]
+      gro_link = node_metadata.get('grounding', {'endpoint': 'x'}).get('endpoint_routing')[0].get('endpoint', 'x').split(':')[0]
       
+      ree_link = node_metadata.get(f'agent_{route}', {'retrieve_embeddings', 'x'}).get('retrieve_embeddings','x').get('endpoint_routing')[0].get('endpoint', 'x').split(':')[0]
+      rag_link = node_metadata.get(f'agent_{route}', {'rag_answer', 'x'}).get('rag_answer','x').get('endpoint_routing')[0].get('endpoint', 'x').split(':')[0]
+
       links.append([
         ref_link, 
         tri_link, 
